f44bfea9c72b563df23da6a93606482e26d3b68a
Lelala <lelala@ischool.com.tw>
2022-02-14 12:21:18 +0800

    新增定義可擴充機制

---
+++ ICalculateRegularAssessmentExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JHEvaluation.Rank
+{
+    public interface ICalculateRegularAssessmentExtension
+    {
+        string Title { get; }
+
+        void Calculate(int batchID);
+    }
+}
